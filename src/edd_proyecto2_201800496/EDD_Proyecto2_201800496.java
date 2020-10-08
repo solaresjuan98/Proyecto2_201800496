7d05bec56f7ad736862323587b287c94b09d8d77
Juan33 <58494935+juansolares333@users.noreply.github.com>
2020-10-08 13:21:31 -0600

    commit

--- src/edd_proyecto2_201800496/EDD_Proyecto2_201800496.java
+++ src/edd_proyecto2_201800496/EDD_Proyecto2_201800496.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package edd_proyecto2_201800496;
 
 /**
@@ -15,7 +11,8 @@ public class EDD_Proyecto2_201800496 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
     }
     
 }
