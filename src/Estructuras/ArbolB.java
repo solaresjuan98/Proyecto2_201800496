075ac9a6c8cb0c097aeeb5a2d3ba64256dca8326
Juan33 <58494935+juansolares333@users.noreply.github.com>
2020-10-09 23:30:12 -0600

    commit

---
+++ src/Estructuras/ArbolB.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Estructuras;
+
+/**
+ *
+ * @author juan333
+ */
+public class ArbolB {
+
+    NodoArbolB raiz;
+    int n;
+
+    public ArbolB(int n) {
+
+        this.n = n;
+        raiz = new NodoArbolB(n);
+    }
+
+    public int buscarClaveMayor() {
+
+        int claveMax = 0;
+        return claveMax;
+
+    }
+
+    public void insertar(int clave) {
+        /*
+            Dos casos 
+            1. Si el nodo esta lleno
+            2. Si el nodo no esta lleno
+         */
+
+        if (raiz.n == 5) {
+
+        } else {
+
+        }
+    }
+
+    public void insertarNoLleno(NodoArbolB nodo, int clave) {
+
+        if (nodo.hoja) {
+
+        } else {
+           int j = 0; 
+        }
+
+    }
+
+}
