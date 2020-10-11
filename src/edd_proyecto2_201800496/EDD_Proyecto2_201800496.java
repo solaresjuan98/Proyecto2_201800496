9ed8d2d7db9718d1fe1dbb43df2153c267795cba
Juan33 <58494935+juansolares333@users.noreply.github.com>
2020-10-10 20:42:09 -0600

    arbol b

--- src/edd_proyecto2_201800496/EDD_Proyecto2_201800496.java
+++ src/edd_proyecto2_201800496/EDD_Proyecto2_201800496.java
@@ -1,5 +1,6 @@
-
 package edd_proyecto2_201800496;
+
+import Estructuras.ArbolB;
 
 /**
  *
@@ -11,8 +12,46 @@ public class EDD_Proyecto2_201800496 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
+
+//        long matrizA[][] = {{0, 3, 4, 999999999, 8, 999999999}, {999999999, 0, 999999999,
+//            999999999, 5, 999999999}, {999999999, 999999999, 0, 999999999, 3, 999999999},
+//        {999999999, 999999999, 999999999, 0, 999999999, 999999999}, {999999999, 999999999,
+//            999999999, 7, 0, 3}, {999999999, 999999999, 999999999, 2, 999999999, 0}};
+//        CaminoMinimo ruta = new CaminoMinimo();
+//
+//        /*for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 3; j++) {
+//                System.out.print(matrizA[i][j]);
+//            }
+//            System.out.println("\n");
+//        }*/
+//        
+//        System.out.println(ruta.algoritmoFloyd(matrizA));
+//    }
+        ArbolB arbol = new ArbolB(3);// no tocar
+
+        int[] valores = {20, 10, 50, 30, 40};
+
+        for (int i = 0; i < valores.length; i++) {
+            arbol.insertar(valores[i]);
+        }
+
+        arbol.insertar(60);
+        arbol.insertar(80);
+        arbol.insertar(70);
+        arbol.insertar(90);
+        arbol.insertar(100);
+        arbol.insertar(4);
+        arbol.insertar(5);
+        arbol.insertar(6);
+        arbol.insertar(7);
+        arbol.insertar(8);
+        arbol.insertar(3);
+        arbol.insertar(15);
+        arbol.insertar(21);
+
+        arbol.mostrarArbolB();
+
     }
-    
+
 }
