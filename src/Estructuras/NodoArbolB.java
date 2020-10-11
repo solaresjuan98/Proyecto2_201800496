f5fc4ba5bcab1ffbc7b830b7c7f1e4135409d86c
Juan33 <58494935+juansolares333@users.noreply.github.com>
2020-10-10 20:42:29 -0600

    arbol b

--- src/Estructuras/NodoArbolB.java
+++ src/Estructuras/NodoArbolB.java
@@ -12,22 +12,32 @@ package Estructuras;
 public class NodoArbolB {
 
     int n;//grado 
-    int maximo = n-1;
-    int gradoMinimo = n / 2;//maximo
+    //int maximo = n - 1;
+    //int gradoMinimo = n / 2;//maximo
     boolean hoja;
-    int clave[];
-    NodoArbolB hijo[];
+    int clave[];// almacenar claves en el nodo
+    NodoArbolB hijo[]; // enlace a referencias a los hijos
 
     //   
-    public NodoArbolB(int n) {
+    public NodoArbolB(int t) {
         n = 0;
         hoja = true;
-        clave = new int[maximo];
-        hijo = new NodoArbolB[maximo];
+        clave = new int[((2 * t) - 1)];
+        //System.out.println(t - 1);
+        hijo = new NodoArbolB[(2 * t)];
     }
 
     public void imprimir() {
+        System.out.print("[");
+        for (int i = 0; i < n; i++) {
+            if (i < n - 1) {
+                System.out.print(clave[i] + " | ");
+            } else {
+                System.out.print(clave[i]);
+            }
+        }
 
+        System.out.print("]");
     }
 
     public int find(int n) {
