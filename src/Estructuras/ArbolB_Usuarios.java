21d400675fea65068352cc8ddb26f9ad9c9f4375
Juan33 <58494935+juansolares333@users.noreply.github.com>
2020-10-12 18:16:58 -0600

    commit

---
+++ src/Estructuras/ArbolB_Usuarios.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Estructuras;
+
+import Clases.Usuario;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author juan333
+ */
+public class ArbolB_Usuarios extends ArbolB {
+
+    ArrayList<Usuario> lista_usuarios = new ArrayList<>();
+
+    public ArbolB_Usuarios(int t) {
+        super(t);
+
+    }
+
+    public void agregarUsuario(Usuario u) {
+        
+        this.insertar(u.getId());
+        lista_usuarios.add(u);
+        
+        
+    }
+}
