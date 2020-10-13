4655825f9d257752cc3fae591d8067e85e21e6fe
Juan33 <58494935+juansolares333@users.noreply.github.com>
2020-10-12 18:16:39 -0600

    commit

---
+++ src/Clases/Usuario.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author juan333
+ */
+public class Usuario {
+    int id;
+    String nombre_completo, username, contrasenia, correo;
+    long telefono;
+
+    public Usuario(int id ,String nombre_completo, String username, String contrasenia, String correo, long telefono) {
+        this.id = id;
+        this.nombre_completo = nombre_completo;
+        this.username = username;
+        this.contrasenia = contrasenia;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre_completo() {
+        return nombre_completo;
+    }
+
+    public void setNombre_completo(String nombre_completo) {
+        this.nombre_completo = nombre_completo;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+
+}
